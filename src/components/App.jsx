827382f91bbae46b485c46c2414cd20abb7b8be9
Stephan Ahlf <stephan.ahlf@cardioscan.de>
2017-11-21 11:55:38 +0100

    change language at runtime without page reload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,7 @@ import {
   Redirect
 } from 'react-router-dom'
 import React, { Component } from 'react';
-
 import routes from './routes.jsx';
-
 import JsonDump from './JsonDump.jsx';
 import Main from './Main.jsx';
 import Tacos from './Tacos.jsx';
@@ -15,25 +13,7 @@ import Bus from './Bus.jsx';
 import Cart from './Cart.jsx';
 import Sandwiches from './Sandwiches.jsx';
 
-
 const locale = require('./language.js')
-
-var T = require('i18n-react').default;
-
-
-var dictionary = {
-  "de-DE": {
-    welcome: "Guten Tag {username}!"
-  },
-  "en-GB": {
-    welcome: "Welcome {username}!"
-  }
-}
-
-// T.setTexts(require('./locales/es.json'));
-// Render component again ... voila in Spanish
-// this.forceUpdate();
-
 
 
 class App extends Component {
@@ -41,13 +21,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { locale: process.env.REACT_APP_LOCALE || locale.id };
-    debugger;
-    // you can combine this solution with markdown!
-    T.setTexts(dictionary[this.state.locale], {
-      notFound: key => `**${key}**` // will render <strong>SomeKey</strong>
-    })
   }
-
 
   render() {
     return (
@@ -57,15 +31,16 @@ class App extends Component {
             <Redirect from="/" exact to={locale.url(this.state.locale, '', this.props)} />
           </Switch>
 
-          <Route path="/:locale" component={Main} />
-          <Route path="/:locale/tacos" component={Tacos} />
-          <Route path="/:locale/tacos/bus" component={Bus} />
-          <Route path="/:locale/tacos/cart" component={Cart} />
-          <Route path="/:locale/sandwiches" component={Sandwiches} />
+          <Route exact path="/:locale" component={Main} />
+          <Route exact path="/:locale/tacos" component={Tacos} />
+          <Route exact path="/:locale/tacos/bus" component={Bus} />
+          <Route exact path="/:locale/tacos/cart" component={Cart} />
+          <Route exact path="/:locale/sandwiches" component={Sandwiches} />
         </div>
       </Router>
     );
   }
+
 }
 
 export default App;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import {
     Link,
     Redirect
 } from 'react-router-dom'
-import React/* , { Component }  */ from 'react';
+import React, { Component } from 'react';
 
 import RouteWithSubRoutes from './RouteWithSubRoutes.jsx';
 import JsonDump from './JsonDump.jsx';
@@ -15,20 +15,63 @@ var T = require('i18n-react').default;
 
 
 
-const Main = (path) => (
-    <div>
-        <h2>Main</h2>
+var dictionary = {
+    "de-DE": {
+        welcome: "Guten Tag {username}!"
+    },
+    "en-GB": {
+        welcome: "Welcome {username}!"
+    }
+}
 
-        <JsonDump data={path.match.params.locale}></JsonDump>
-        <div>
-            <strong>{T.translate("welcome", { username: "Bruce Wayne" })}</strong>
-            <ul>
-                <li><Link to={locale.url(path.match.params.locale, 'tacos')}>tacos</Link></li>
-                <li><Link to={locale.url(path.match.params.locale, 'sandwiches')}>Sandwiches</Link></li>
-            </ul>
+// T.setTexts(require('./locales/es.json'));
+// Render component again ... voila in Spanish
+// this.forceUpdate();
 
-        </div>
 
-    </div>
-)
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { locale: this.props.match.params.locale };
+
+        // you can combine this solution with markdown!
+        T.setTexts(dictionary[this.state.locale], {
+            notFound: key => `**${key}**` // will render <strong>SomeKey</strong>
+        })
+    }
+
+    onClick(e) {
+        T.setTexts(dictionary[e.target.dataset.locale], {
+            notFound: key => `**${key}**` // will render <strong>SomeKey</strong>
+        })
+        this.setState({ locale: e.target.dataset.locale })
+    }
+
+
+    render(path) {
+        return (
+            <div>
+                <ul>
+                    <li><Link data-locale={'de-DE'} onClick={this.onClick.bind(this)} to={locale.url('de-DE', '')}>Deutsch</Link></li>
+                    <li><Link data-locale={'en-GB'} onClick={this.onClick.bind(this)} to={locale.url('en-GB', '')}>English</Link></li>
+                </ul>
+
+                <h2>Main</h2>
+
+                <JsonDump data={this.state.locale}></JsonDump>
+                <div>
+                    <strong>{T.translate("welcome", { username: "Bruce Wayne" })}</strong>
+                    <ul>
+                        <li><Link to={locale.url(this.state.locale, 'tacos')}>tacos</Link></li>
+                        <li><Link to={locale.url(this.state.locale, 'sandwiches')}>Sandwiches</Link></li>
+                    </ul>
+
+                </div>
+
+            </div>
+        )
+    }
+}
 export default Main;
